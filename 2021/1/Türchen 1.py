4fc01d5a07f20f14687ec35acf241a4407b54a2e
Maurice-Pascal L <larivieremauricepascal112@gmail.com>
2021-12-02 15:44:19 +0100

    Updated something

--- 2021/1/Türchen 1.py
+++ 2021/1/Türchen 1.py
@@ -1,3 +1,16 @@
+"""
+Die Aufgabe des Türchen ist es, eine 2021x .zip-verpackte Datei so oft zu entpacken,
+bis man die Ursprungstextdatei hat, in der das Passwort für das Video steht.
+
+Video von Algorithmen verstehen: https://www.youtube.com/watch?v=G3vLQP5yAKQ
+Artikel: https://www.floriandalwigk.de/santas-geschenk-hackcember-1
+
+Erstellt am 01.12.2021 (dd.MM.yyyy) mit PyCharm Community
+
+Mit freundlichen Grüßen
+Maurice-Pascal
+"""
+
 import zipfile
 import os
 
